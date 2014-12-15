1ebd7b21acffbe79fdab81a4e59bf679f154eb49
“PrinceSyed” <ibrahim48089@yahoo.com>
2014-12-14 23:20:33 -0500

    dfdfd

---
+++ app/views/hello.php
@@ -0,0 +1,14 @@
+<?php include_once "includes/nav.php"; ?>
+
+	<div class="welcome">
+        <img src="http://www.seekmomentum.com/wp-content/themes/momentum/images/logo.png" alt="Momentum Ping Pong App">
+		<h1>Let's Build a Ping Pong App</h1>
+	</div>
+
+<?php if(DB::connection()->getDatabaseName())
+{
+   echo "connected sucessfully to database ".DB::connection()->getDatabaseName();
+}
+?>
+
+<?php include_once "includes/footer.php"; ?>
